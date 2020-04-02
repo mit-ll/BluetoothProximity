4be21e9b1cb372a98247ce6bff2327d3ae3a1984
Michael Wentz <michael.wentz@ll.mit.edu>
2020-04-02 18:04:00 -0400

    Initial commit - print hello world
    Prints hello world to the console.

---
+++ Bluetooth Proximity/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Bluetooth Proximity
+//
+//  Created by Michael Wentz on 4/2/20.
+//  Copyright © 2020 Michael Wentz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("Hello world")
+    }
+
+
+}
+
