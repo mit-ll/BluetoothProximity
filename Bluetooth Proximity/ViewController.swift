ce26a6f334f9f67ef9c8a8bf4573ef9262f0db9f
Michael Wentz <michael.wentz@ll.mit.edu>
2020-04-02 18:22:51 -0400

    Add basic bluetooth logging
    Saves timestamp, RSSI, and advertisement data. Made by following this tutorial: https://scribles.net/creating-a-simple-ble-scanner-on-iphone/

--- Bluetooth Proximity/ViewController.swift
+++ Bluetooth Proximity/ViewController.swift
@@ -7,15 +7,50 @@
 //
 
 import UIKit
+import CoreBluetooth
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CBCentralManagerDelegate, CLLocationManagerDelegate {
+    
+    // Variables
+    var centralManager : CBCentralManager!
+    var count = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        print("Hello world")
+        // Primary setup
+        centralManager = CBCentralManager(delegate: self, queue: nil, options: nil)
     }
-
-
+    
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        if central.state == .poweredOn {
+            print("Bluetooth is on")
+            centralManager.scanForPeripherals(withServices: nil, options: nil)
+        } else {
+            print("Bluetooth is off!")
+        }
+    }
+    
+    // Prints timestamp
+    func printTime() {
+        let date = Date()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss.SSS"
+        print("Time : " + formatter.string(from: date))
+    }
+    
+    public func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        
+        // Print the current count, time, UUID, RSSI, name, and any advertised data
+        count += 1
+        print("Count : " + count.description)
+        printTime()
+        print("UUID : \(peripheral.identifier)")
+        print("RSSI : \(RSSI)")
+        print("Name : \(peripheral.name ?? "None")")
+        for (i,j) in advertisementData {
+            print("\(i) : \(j)")
+        }
+    }
+    
 }
-
