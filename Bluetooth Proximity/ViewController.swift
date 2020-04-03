42bd00164127be07e5fa545be9322604bcbd82c6
Michael Wentz <michael.wentz@ll.mit.edu>
2020-04-02 23:33:53 -0400

    Add initial M-of-N detector and UI
    Have not debugged/validated the detector code much, but seems to be working on target. M, N, and RSSI threshold set arbitrarily. And I'm sure there is a better way to put text on the screen than the way I'm doing it...

--- Bluetooth Proximity/ViewController.swift
+++ Bluetooth Proximity/ViewController.swift
@@ -11,31 +11,91 @@ import CoreBluetooth
 
 class ViewController: UIViewController, CBCentralManagerDelegate {
     
+    // Parameters
+    var scanEverySec = 0.5          // How often the scan is restarted  (seconds)
+    var rssiThresh = -65            // Threshold RSSI, dBm
+    var M = 20                      // Samples that must cross threshold
+    var N = 50                      // Total number of samples
+    
+    // Debugging
+    var printDebug = false
+    
     // Variables
     var centralManager : CBCentralManager!
     var count = 0
-    // How often to restart the scan (in seconds)
-    var scanEverySec = 0.5
     var scanTimer = Timer()
+    var uuids : [String] = []
+    var mMtx : [[Int]] = []
+    var mPtr : [Int] = []
+    var nArr : [Int] = []
+    var detArr : [Bool] = []
+    
+    // Outlets
+    @IBOutlet weak var statusText: UILabel!
+    @IBOutlet weak var countText: UILabel!
+    
+    @IBOutlet weak var uuidLabel0: UILabel!
+    @IBOutlet weak var uuidLabel1: UILabel!
+    @IBOutlet weak var uuidLabel2: UILabel!
+    @IBOutlet weak var uuidLabel3: UILabel!
+    @IBOutlet weak var uuidLabel4: UILabel!
+    @IBOutlet weak var uuidLabel5: UILabel!
+    @IBOutlet weak var uuidLabel6: UILabel!
+    @IBOutlet weak var uuidLabel7: UILabel!
+    @IBOutlet weak var uuidLabel8: UILabel!
+    @IBOutlet weak var uuidLabel9: UILabel!
+    var uuidLabelArr : [UILabel] = []
+    
+    @IBOutlet weak var rssiLabel0: UILabel!
+    @IBOutlet weak var rssiLabel1: UILabel!
+    @IBOutlet weak var rssiLabel2: UILabel!
+    @IBOutlet weak var rssiLabel3: UILabel!
+    @IBOutlet weak var rssiLabel4: UILabel!
+    @IBOutlet weak var rssiLabel5: UILabel!
+    @IBOutlet weak var rssiLabel6: UILabel!
+    @IBOutlet weak var rssiLabel7: UILabel!
+    @IBOutlet weak var rssiLabel8: UILabel!
+    @IBOutlet weak var rssiLabel9: UILabel!
+    var rssiLabelArr : [UILabel] = []
+    
+    @IBOutlet weak var proximityLabel0: UILabel!
+    @IBOutlet weak var proximityLabel1: UILabel!
+    @IBOutlet weak var proximityLabel2: UILabel!
+    @IBOutlet weak var proximityLabel3: UILabel!
+    @IBOutlet weak var proximityLabel4: UILabel!
+    @IBOutlet weak var proximityLabel5: UILabel!
+    @IBOutlet weak var proximityLabel6: UILabel!
+    @IBOutlet weak var proximityLabel7: UILabel!
+    @IBOutlet weak var proximityLabel8: UILabel!
+    @IBOutlet weak var proximityLabel9: UILabel!
+    var proximityLabelArr : [UILabel] = []
     
     // Primary setup
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Pack labels into arrays
+        uuidLabelArr = [uuidLabel0, uuidLabel1, uuidLabel2, uuidLabel3, uuidLabel4, uuidLabel5, uuidLabel6, uuidLabel7, uuidLabel8, uuidLabel9]
+        rssiLabelArr = [rssiLabel0, rssiLabel1, rssiLabel2, rssiLabel3, rssiLabel4, rssiLabel5, rssiLabel6, rssiLabel7, rssiLabel8, rssiLabel9]
+        proximityLabelArr = [proximityLabel0, proximityLabel1, proximityLabel2, proximityLabel3, proximityLabel4, proximityLabel5, proximityLabel6, proximityLabel7, proximityLabel8, proximityLabel9]
+        
+        // Make the bluetooth manager
         centralManager = CBCentralManager(delegate: self, queue: nil, options: nil)
-    }
-    
-    // Print a debug message
-    func printDebug(_ message : String){
-        print("[DEBUG] " + message)
     }
     
     // Start scanning
     func centralManagerDidUpdateState(_ central: CBCentralManager) {
         if central.state == .poweredOn {
-            printDebug("Bluetooth is on, starting scans")
+            statusText.text = "Running!"
+            if printDebug {
+                print("Bluetooth is on, starting scans")
+            }
             scanTimerLoop()
         } else {
-            printDebug("Bluetooth is off!")
+            statusText.text = "Bluetooth is off!"
+            if printDebug {
+                print("Bluetooth is off!")
+            }
         }
     }
     
@@ -46,7 +106,9 @@ class ViewController: UIViewController, CBCentralManagerDelegate {
     
     // Restarts the scan
     @objc func restartScan() {
-        printDebug("Restarting scan")
+        if printDebug {
+            print("Restarting scan")
+        }
         centralManager.stopScan()
         centralManager.scanForPeripherals(withServices: nil, options: nil)
     }
@@ -63,14 +125,63 @@ class ViewController: UIViewController, CBCentralManagerDelegate {
         
         // Print the current count, time, UUID, RSSI, name, and any advertised data
         count += 1
-        print("Count : " + count.description)
-        printTime()
-        print("UUID : \(peripheral.identifier)")
-        print("RSSI : \(RSSI)")
-        print("Name : \(peripheral.name ?? "None")")
-        for (i,j) in advertisementData {
-            print("\(i) : \(j)")
+        let uuid = peripheral.identifier.uuidString
+        if printDebug {
+            print("Count : " + count.description)
+            printTime()
+            print("UUID : " + uuid)
+            print("RSSI : \(RSSI)")
+            print("Name : \(peripheral.name ?? "None")")
+            for (i,j) in advertisementData {
+                print("\(i) : \(j)")
+            }
         }
+        
+        // If we haven't seen this UUID, set up storage for it
+        var uuidIdx = uuids.index(of: uuid)
+        if uuidIdx == nil {
+            uuids.append(uuid)
+            uuidIdx = uuids.count - 1
+            mMtx.append([Int](repeating: 0, count: M))
+            mPtr.append(0)
+            nArr.append(0)
+            detArr.append(false)
+        }
+        
+        // M-of-N detector. This makes a decision for each sample based on the threshold.
+        // If there are at least N samples, and M of the decisions declare detection, then
+        // overall detection is declared.
+        if nArr[uuidIdx!] < N {
+            nArr[uuidIdx!] += 1
+        }
+        if RSSI.intValue >= rssiThresh {
+            mMtx[uuidIdx!][mPtr[uuidIdx!]] = 1
+        } else {
+            mMtx[uuidIdx!][mPtr[uuidIdx!]] = 0
+        }
+        mPtr[uuidIdx!] += 1
+        if mPtr[uuidIdx!] == M {
+            mPtr[uuidIdx!] = 0
+        }
+        let s = mMtx[uuidIdx!].reduce(0, +)
+        if (s >= M) && (nArr[uuidIdx!] == N) {
+            detArr[uuidIdx!] = true
+        } else {
+            detArr[uuidIdx!] = false
+        }
+        
+        // Update screen
+        countText.text = count.description
+        uuidLabelArr[uuidIdx!].text = String(uuid.prefix(4))
+        rssiLabelArr[uuidIdx!].text = RSSI.description
+        if detArr[uuidIdx!] {
+            proximityLabelArr[uuidIdx!].text = "Close"
+            proximityLabelArr[uuidIdx!].textColor = UIColor.red
+        } else {
+            proximityLabelArr[uuidIdx!].text = "Far"
+            proximityLabelArr[uuidIdx!].textColor = UIColor.green
+        }
+
     }
     
 }
