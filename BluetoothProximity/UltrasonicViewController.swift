f875f032aff38dcf42afd5ff9e52c629c24ab842
Michael Wentz <michael.wentz@ll.mit.edu>
2020-05-07 19:39:13 -0400

    Ultrasonic: add transmitting a tone
    - Currently transmits a 0.2 second 20 kHz tone every second
    - Added class to write floating point samples to a binary file (so we can get data off the phone for analysis)
    - During development, noticed that if the argument to the sin function is floating point, there seems to be spectral regrowth that gets worse over time when playing out of actual iPhone hardware. Changing the argument to double precision fixes this.

--- BluetoothProximity/UltrasonicViewController.swift
+++ BluetoothProximity/UltrasonicViewController.swift
@@ -7,8 +7,13 @@
 //
 
 import UIKit
+import AVFoundation
 
 class UltrasonicViewController: UIViewController {
+    
+    // Objects
+    var tx: audioTx!
+    var engine: AVAudioEngine!
     
     // Make status bar light
     override var preferredStatusBarStyle: UIStatusBarStyle {
@@ -17,9 +22,24 @@ class UltrasonicViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+                
         // Initialize
+        tx = audioTx()
+        engine = AVAudioEngine()
         isRunning = false
+        
+        // Connect components
+        engine.attach(tx.player)
+        engine.connect(tx.player, to:engine.outputNode, format: tx.buff.format)
+        
+        // Startup
+        do {
+            try engine.start()
+        } catch {
+            #if DEBUG
+            print("AVAudioEngine failed to start")
+            #endif
+        }
     }
     
     // Run/stop button
@@ -35,10 +55,9 @@ class UltrasonicViewController: UIViewController {
     
     // Starts running
     func startRun() {
-        
-        #if DEBUG
-        print("Start run")
-        #endif
+
+        // Start transmitter
+        tx.startLoop()
         
         // Update UI
         runStopButton.setTitle("Stop", for: .normal)
@@ -49,16 +68,128 @@ class UltrasonicViewController: UIViewController {
     
     // Stops running
     func stopRun() {
-        
-        #if DEBUG
-        print("Stop run")
-        #endif
+
+        // Stop transmitter
+        tx.stopLoop()
         
         // Update UI
         runStopButton.setTitle("Run", for: .normal)
         
         // Update state
         isRunning = false
+    }
+    
+}
+
+// Writes data samples to file
+class dataWriter {
+    
+    // Objects
+    var fileManager: FileManager!
+    var fileHandle: FileHandle!
+    var fileURL: URL!
+    
+    // Initialize
+    init() {
+        fileManager = FileManager.default
+    }
+    
+    // Gets directory to save data
+    func getDir() -> URL {
+        let paths = fileManager.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
+    
+    // Creates a new file
+    func createFile(fileName: String) {
+        fileURL = getDir().appendingPathComponent(fileName)
+        fileManager.createFile(atPath: fileURL.path, contents: nil, attributes: nil)
+        do {
+            try fileHandle = FileHandle(forUpdating: fileURL)
+        }
+        catch {
+            #if DEBUG
+            print("Failed to create FileHandle")
+            #endif
+        }
+    }
+    
+    // Writes an array of samples to file
+    func writeArray(data: [Float32]) {
+        fileHandle.seekToEndOfFile()
+        let tmp = data.withUnsafeBytes{Array($0)}
+        fileHandle.write(Data(tmp))
+    }
+    
+}
+
+// Transmitter - sends signals
+class audioTx {
+    
+    // Objects
+    var buff: AVAudioPCMBuffer!
+    var player: AVAudioPlayerNode!
+    var sendTimer: Timer?
+    var repeatEvery: Double!
+    
+    // Initialize
+    init() {
+        
+        // Sample rate (samples/second) and duration (seconds)
+        let fs: Double = 48000.0
+        let d: Double = 0.2
+        
+        // How often to repeat (in seconds)
+        repeatEvery = 1.0
+        
+        // Buffer size
+        let buffSize = AVAudioFrameCount(fs*d)
+        
+        // Create a tone
+        let a: Double = 1.0
+        let f: Double = 20000.0
+        let pi: Double = Double.pi
+        let n = Int(buffSize)
+        var x: [Float32] = Array(repeating: 0.0, count: n)
+        for i in 0...Int(buffSize-1) {
+            // The argument is a Double and we cast to Float32 (for a cleaner sin wave
+            // compared to the argument also being Float32).
+            x[i] = Float32(a*sin(2.0*pi*(f/fs)*Double(i)))
+        }
+        
+        // Save to file
+        //let writer = dataWriter()
+        //writer.createFile(fileName: "tx.dat")
+        //writer.writeArray(data: x)
+                
+        // Set up buffer to send
+        let audioFormat = AVAudioFormat(standardFormatWithSampleRate: fs, channels: 2)!
+        buff = AVAudioPCMBuffer(pcmFormat: audioFormat, frameCapacity: buffSize)!
+        buff.frameLength = buffSize
+        buff.floatChannelData![0].initialize(from: &x, count: n)
+        buff.floatChannelData![1].initialize(from: &x, count: n)
+        
+        // Create audio player
+        player = AVAudioPlayerNode()
+    }
+    
+    // Start transmitting in a loop
+    func startLoop() {
+        sendTimer = Timer.scheduledTimer(timeInterval: repeatEvery, target: self, selector: #selector(audioTx.send), userInfo: nil, repeats: true)
+        sendTimer?.fire()
+    }
+    
+    // Transmit once
+    @objc func send() {
+        player.scheduleBuffer(buff)
+        player.play()
+    }
+    
+    // Stop transmitting in a loop
+    func stopLoop() {
+        sendTimer?.invalidate()
+        sendTimer = nil
+        player.stop()
     }
     
 }
