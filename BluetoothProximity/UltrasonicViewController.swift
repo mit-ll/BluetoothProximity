ac67c21f7f64a0a37bae3b3f8eb4d2055b234757
Michael Wentz <michael.wentz@ll.mit.edu>
2020-06-04 16:19:19 -0400

    Ultrasonic: make separate master/slave waveforms
    Each device makes both waveforms so they can do the measurements. Currently only loopback delay is measured and transferred over BLE. It seems to work most of the time, but sometimes the counter on the slave gets ahead by one and it doesn't show it's own measurement - not quite sure why. There were also crashes when trying to run too soon after the last run was done, that's mostly fixed, although I haven't tested it enough to be super confident in it.

--- BluetoothProximity/UltrasonicViewController.swift
+++ BluetoothProximity/UltrasonicViewController.swift
@@ -18,8 +18,8 @@ struct ultrasonicData {
     static var remoteTime: Double = 0
     static var remoteTimeValid: Bool = false
     
-    static var localTxSamples: [Float32] = [0]
-    static var remoteTxSamples: [Float32] = [0]
+    static var masterTxSamples: [Float32] = [0]
+    static var slaveTxSamples: [Float32] = [0]
     
     static var sendTime: Double = 0
     static var recvSelfTime: Double = 0
@@ -77,7 +77,7 @@ class UltrasonicViewController: UIViewController {
         
         // Connect transmitter
         engine.attach(tx.player)
-        engine.connect(tx.player, to:engine.outputNode, format: tx.buff.format)
+        engine.connect(tx.player, to:engine.outputNode, format: tx.masterBuff.format)
         
         // Startup
         do {
@@ -163,8 +163,8 @@ class UltrasonicViewController: UIViewController {
         // Update state
         isRunning = true
         
-        // Stop running after 500 ms
-        DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(500), execute: {
+        // Stop running after 1 second
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(1), execute: {
             self.stopRun()
         })
     }
@@ -191,32 +191,35 @@ class UltrasonicViewController: UIViewController {
         scanner.setName(name: "uMeas")
         scanner.startScanForService()
         
-        // After 500 ms, stop scanning and compute range
-        // If we're the slave, return to search for the start signal
+        // Wait 500 ms before continuing
         DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(500), execute: {
+            
+            // Stop scanning. If we're the slave, return to search for the start signal
             self.scanner.stop()
             if self.isSlave {
                 self.scanner.setName(name: "uStart")
                 self.scanner.startScanForService()
             }
+            
+            // Compute range
             self.twoWayRanging()
+            
+            // Update UI
+            if self.isSlave {
+                self.runStopButton.setTitle("Waiting...", for: .normal)
+            } else {
+                self.runStopButton.isEnabled = true
+                self.runStopButton.setTitle("Run", for: .normal)
+            }
+            self.rangeStepper.isEnabled = true
+            self.masterSlaveControl.isEnabled = true
+            
+            // Update state
+            self.isRunning = false
+            
+            // Update run counter label
+            self.countLabel.text = self.count.description
         })
-        
-        // Update UI
-        if isSlave {
-            runStopButton.setTitle("Waiting...", for: .normal)
-        } else {
-            runStopButton.isEnabled = true
-            runStopButton.setTitle("Run", for: .normal)
-        }
-        rangeStepper.isEnabled = true
-        masterSlaveControl.isEnabled = true
-        
-        // Update state
-        isRunning = false
-        
-        // Update run counter label
-        countLabel.text = count.description
     }
     
     // Stop any run when we leave the tab
@@ -347,7 +350,8 @@ func xcorr(a: [Float32], b: [Float32]) -> ([Float32], [Int]) {
 class audioTx {
     
     // Objects
-    var buff: AVAudioPCMBuffer!
+    var masterBuff: AVAudioPCMBuffer!
+    var slaveBuff: AVAudioPCMBuffer!
     var player: AVAudioPlayerNode!
     var writer: dataWriter!
     
@@ -383,24 +387,29 @@ class audioTx {
         // Transition band is 17-18 kHz (everything above 18 kHz is passed)
         let b: [Float32] = [-0.000236437942135332,0.000815325587435857,-0.00183656749123850,0.00322373502187833,-0.00457968516861099,0.00537189644388272,-0.00505499229413463,0.00347779908067731,-0.000994138199842880,-0.00149735453578532,0.00299215894167622,-0.00281888001320860,0.00112282019378742,0.00117742109999269,-0.00276190993327034,0.00268673229611326,-0.000919027237732912,-0.00151847017808169,0.00313502230989920,-0.00282316058926014,0.000639139330387021,0.00216118221188974,-0.00378479893826083,0.00305407445743594,-0.000213098251323078,-0.00305509803583764,0.00462083414717519,-0.00325156533384849,-0.000442882872856017,0.00423976399299556,-0.00559353103863132,0.00332987943691472,0.00143089652234904,-0.00575220869787921,0.00667203811847967,-0.00318132338188699,-0.00285718529266932,0.00765172352815033,-0.00780739875056750,0.00268031096794725,0.00487600364443114,-0.0100153958122534,0.00895157284903807,-0.00164111991850756,-0.00771526406578997,0.0129957257493104,-0.0100525527756791,-0.000208683315191778,0.0117728338217874,-0.0168823062953845,0.0110573662379157,0.00338991198315377,-0.0178761268941901,0.0223633756310119,-0.0118999586500315,-0.00913092301919785,0.0282155195419526,-0.0314611418824086,0.0125464824690390,0.0213641817308486,-0.0508192925894373,0.0527952804647337,-0.0129465536996555,-0.0650313835263956,0.158401873393614,-0.234059009950929,0.263085660805954,-0.234059009950929,0.158401873393614,-0.0650313835263956,-0.0129465536996555,0.0527952804647337,-0.0508192925894373,0.0213641817308486,0.0125464824690390,-0.0314611418824086,0.0282155195419526,-0.00913092301919785,-0.0118999586500315,0.0223633756310119,-0.0178761268941901,0.00338991198315377,0.0110573662379157,-0.0168823062953845,0.0117728338217874,-0.000208683315191778,-0.0100525527756791,0.0129957257493104,-0.00771526406578997,-0.00164111991850756,0.00895157284903807,-0.0100153958122534,0.00487600364443114,0.00268031096794725,-0.00780739875056750,0.00765172352815033,-0.00285718529266932,-0.00318132338188699,0.00667203811847967,-0.00575220869787921,0.00143089652234904,0.00332987943691472,-0.00559353103863132,0.00423976399299556,-0.000442882872856017,-0.00325156533384849,0.00462083414717519,-0.00305509803583764,-0.000213098251323078,0.00305407445743594,-0.00378479893826083,0.00216118221188974,0.000639139330387021,-0.00282316058926014,0.00313502230989920,-0.00151847017808169,-0.000919027237732912,0.00268673229611326,-0.00276190993327034,0.00117742109999269,0.00112282019378742,-0.00281888001320860,0.00299215894167622,-0.00149735453578532,-0.000994138199842880,0.00347779908067731,-0.00505499229413463,0.00537189644388272,-0.00457968516861099,0.00322373502187833,-0.00183656749123850,0.000815325587435857,-0.00023643794213533]
         
+        // -------------------------------------------------------------------------------------
+        // Master TX waveform
+        // -------------------------------------------------------------------------------------
+        
         // Random number generator seed based on UDID
-        let devStr = UIDevice.current.identifierForVendor?.uuidString
-        let devSeed: UInt64 = strHash(devStr!)
+        //let devStr = UIDevice.current.identifierForVendor?.uuidString
+        //let devSeed: UInt64 = strHash(devStr!)
         
         // Create white noise (as integers, then scaled to floats)
-        let rng = GKMersenneTwisterRandomSource(seed: devSeed)
+        let masterSeed: UInt64 = strHash("master123")
+        let masterRng = GKMersenneTwisterRandomSource(seed: masterSeed)
         let stdDev = Float32(65536.0)
-        let randn = GKGaussianDistribution(randomSource: rng, mean: 0, deviation: stdDev)
+        let masterRandn = GKGaussianDistribution(randomSource: masterRng, mean: 0, deviation: stdDev)
         let n = Int(buffSize)
         let n_pad = b.count
         var x: [Float32] = Array(repeating: 0.0, count: (n + n_pad))
         for i in 0...(n-1) {
-            x[i] = Float32(randn.nextInt())/stdDev
+            x[i] = Float32(masterRandn.nextInt())/stdDev
         }
         
         // High pass filter
         if #available(iOS 13.0, *) {
-            ultrasonicData.localTxSamples = vDSP.convolve(x, withKernel: b)
+            ultrasonicData.masterTxSamples = vDSP.convolve(x, withKernel: b)
         } else {
             // Fallback on earlier versions
             #if DEBUG
@@ -409,9 +418,9 @@ class audioTx {
         }
 
         // Normalize to +/- 1
-        let m = Float32(ultrasonicData.localTxSamples.max()!)
-        ultrasonicData.localTxSamples.enumerated().forEach { i, v in
-            ultrasonicData.localTxSamples[i] = v/m
+        var m = Float32(ultrasonicData.masterTxSamples.max()!)
+        ultrasonicData.masterTxSamples.enumerated().forEach { i, v in
+            ultrasonicData.masterTxSamples[i] = v/m
         }
         
         // Ramp up and down
@@ -419,15 +428,59 @@ class audioTx {
         let dRamp = 2.5e-3
         let nRamp = Int(fs*dRamp)
         for i in 0...(nRamp-1) {
-            ultrasonicData.localTxSamples[i] *= Float32(i)/Float32(nRamp)
-            ultrasonicData.localTxSamples[Int(buffSize)-i-1] *= Float32(i)/Float32(nRamp)
+            ultrasonicData.masterTxSamples[i] *= Float32(i)/Float32(nRamp)
+            ultrasonicData.masterTxSamples[Int(buffSize)-i-1] *= Float32(i)/Float32(nRamp)
         }
         
         // Set up buffer to send
         let audioFormat = AVAudioFormat(standardFormatWithSampleRate: fs, channels: 1)!
-        buff = AVAudioPCMBuffer(pcmFormat: audioFormat, frameCapacity: buffSize)!
-        buff.frameLength = buffSize
-        buff.floatChannelData![0].initialize(from: &ultrasonicData.localTxSamples, count: n)
+        masterBuff = AVAudioPCMBuffer(pcmFormat: audioFormat, frameCapacity: buffSize)!
+        masterBuff.frameLength = buffSize
+        masterBuff.floatChannelData![0].initialize(from: &ultrasonicData.masterTxSamples, count: n)
+        
+        // -------------------------------------------------------------------------------------
+        // Slave TX waveform
+        // -------------------------------------------------------------------------------------
+        
+        // Create white noise (as integers, then scaled to floats)
+        let slaveSeed: UInt64 = strHash("slave456")
+        let slaveRng = GKMersenneTwisterRandomSource(seed: slaveSeed)
+        let slaveRandn = GKGaussianDistribution(randomSource: slaveRng, mean: 0, deviation: stdDev)
+        x = Array(repeating: 0.0, count: (n + n_pad))
+        for i in 0...(n-1) {
+            x[i] = Float32(slaveRandn.nextInt())/stdDev
+        }
+        
+        // High pass filter
+        if #available(iOS 13.0, *) {
+            ultrasonicData.slaveTxSamples = vDSP.convolve(x, withKernel: b)
+        } else {
+            // Fallback on earlier versions
+            #if DEBUG
+            print("vDSP.convolve is not available")
+            #endif
+        }
+
+        // Normalize to +/- 1
+        m = Float32(ultrasonicData.slaveTxSamples.max()!)
+        ultrasonicData.slaveTxSamples.enumerated().forEach { i, v in
+            ultrasonicData.slaveTxSamples[i] = v/m
+        }
+        
+        // Ramp up and down
+        for i in 0...(nRamp-1) {
+            ultrasonicData.slaveTxSamples[i] *= Float32(i)/Float32(nRamp)
+            ultrasonicData.slaveTxSamples[Int(buffSize)-i-1] *= Float32(i)/Float32(nRamp)
+        }
+        
+        // Set up buffer to send
+        slaveBuff = AVAudioPCMBuffer(pcmFormat: audioFormat, frameCapacity: buffSize)!
+        slaveBuff.frameLength = buffSize
+        slaveBuff.floatChannelData![0].initialize(from: &ultrasonicData.slaveTxSamples, count: n)
+        
+        // -------------------------------------------------------------------------------------
+        // Finish audio setup
+        // -------------------------------------------------------------------------------------
         
         // Create audio player
         player = AVAudioPlayerNode()
@@ -459,8 +512,13 @@ class audioTx {
         writer.createFile(fileName: txFile)
         
         // Save samples to file
-        writer.writeDoubleArray(data: [Double(ultrasonicData.localTxSamples.count)])
-        writer.writeFloatArray(data: ultrasonicData.localTxSamples)
+        if isSlave {
+            writer.writeDoubleArray(data: [Double(ultrasonicData.slaveTxSamples.count)])
+            writer.writeFloatArray(data: ultrasonicData.slaveTxSamples)
+        } else {
+            writer.writeDoubleArray(data: [Double(ultrasonicData.masterTxSamples.count)])
+            writer.writeFloatArray(data: ultrasonicData.masterTxSamples)
+        }
                 
         // Master sends 50 ms from now, slave 250 ms from now
         var info = mach_timebase_info()
@@ -486,8 +544,13 @@ class audioTx {
         
         // Schedule and playout
         player.stop()
-        player.scheduleBuffer(buff)
-        player.prepare(withFrameCount: buff.frameLength)
+        if isSlave {
+            player.scheduleBuffer(slaveBuff)
+            player.prepare(withFrameCount: slaveBuff.frameLength)
+        } else {
+            player.scheduleBuffer(masterBuff)
+            player.prepare(withFrameCount: masterBuff.frameLength)
+        }
         player.play(at: sendAvTime)
     }
     
@@ -543,25 +606,31 @@ class audioRx {
             self.recorder.removeTap(onBus: 0)
 
             // Process data
-            self.processData(x: Array(buff), t: nanos)
+            self.processData(isSlave: isSlave, x: Array(buff), t: nanos)
         }
         
     }
     
     // Receiver processing
-    func processData(x: [Float32], t: Double) {
+    func processData(isSlave: Bool, x: [Float32], t: Double) {
         
         #if DEBUG
         //print("Received \(x.count) samples at \(t)")
         #endif
-                
+        
         // Save to file
         let md = [t, Double(x.count)]
         writer.writeDoubleArray(data: md)
         writer.writeFloatArray(data: x)
         
         // Crosscorrelation with self
-        var (y, yLags) = xcorr(a: x, b: ultrasonicData.localTxSamples)
+        var y: [Float32]
+        var yLags: [Int]
+        if isSlave {
+            (y, yLags) = xcorr(a: x, b: ultrasonicData.slaveTxSamples)
+        } else {
+            (y, yLags) = xcorr(a: x, b: ultrasonicData.masterTxSamples)
+        }
         y = y.map(abs)
         var selfIdx: UInt
         if #available(iOS 13.0, *) {
@@ -576,9 +645,9 @@ class audioRx {
         
         // Loopback delay
         let tDelta = (tSelf + t) - ultrasonicData.sendTime
-                
+        
         // Mark measurement as valid
-        ultrasonicData.localTime = tDelta/1e9 // temporary
+        ultrasonicData.localTime = tDelta // temporary
         ultrasonicData.localTimeValid = true
     }
     
